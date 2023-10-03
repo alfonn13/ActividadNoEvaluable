8e6fda1739a80c1a1589b44fb4f928f977609ac2
unknown <alfonsoreviejovalle@alumnos.tuniversformacion.es>
2023-10-03 10:34:47 +0200

    Actualizacion

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+Alfonso Reviejo Valle
+GitHub: 
+*/
+
+const numero = prompt("Ingrese el numero: ");
+
+if(numero>0){
+    alert(`El numero ${numero} ingresado es positivo`);
+    console.log(`El numero ${numero} es positivo`);
+}else if(num<0){
+    alert(`El numero ${numero} ingresado es negativo`);
+    console.log(`El numero ${numero} es negativo`);
+}else{
+    alert(`El numero  ${numero} ingresado es igual a 0`);
+    console.log(`El numero ${numero} es igual a 0`)
+
+}
